0803c200d44315bd3a11330796d9d3a0d4297458
Dipasha <120773490+DipashaK@users.noreply.github.com>
2024-12-09 21:06:52 +0530

    Update donation.js

--- server/Models/donation.js
+++ server/Models/donation.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 
 const donationSchema = new mongoose.Schema({
   donorName: { type: String, required: true },
-  phone: { type: String, required: true },
-  email: { type: String, required: true },
-  gender: { type: String, required: true },
+  phone: { type: String, required: true, match: /^[0-9]{10}$/ },
+  email: { type: String, required: true, match: /^[a-zA-Z0-9._%+-]+@gmail\.com$/ },
+  gender: { type: String, required: true, enum: ['Male', 'Female', 'Other'] },
   organ: { type: String, required: true },
-  bloodGroup: { type: String, required: true },
+  bloodGroup: { type: String, required: true, enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'] },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  matchedWith: { type: mongoose.Schema.Types.ObjectId, ref: 'Receiver' },
 });
 
 module.exports = mongoose.model('Donation', donationSchema);
