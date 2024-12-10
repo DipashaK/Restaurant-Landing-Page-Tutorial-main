6916d79403ee06bf260001066c7663cf1213cf9e
Dipasha <120773490+DipashaK@users.noreply.github.com>
2024-12-10 11:36:38 +0530

    Update transplant.js

--- server/Models/transplant.js
+++ server/Models/transplant.js
@@ -1,4 +1,18 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+
+// const transplantSchema = new mongoose.Schema({
+//   date: { type: String, required: true },
+//   time: { type: String, required: true },
+//   organ: { type: String, required: true },
+//   recipient: { type: String, required: true },
+//   location: { type: String, required: true },
+// });
+
+// const Transplant = mongoose.model('Transplant', transplantSchema);
+
+// module.exports = Transplant;
+
+const mongoose = require("mongoose");
 
 const transplantSchema = new mongoose.Schema({
   date: { type: String, required: true },
@@ -6,8 +20,7 @@ const transplantSchema = new mongoose.Schema({
   organ: { type: String, required: true },
   recipient: { type: String, required: true },
   location: { type: String, required: true },
+  endDate: { type: String, required: true },
 });
 
-const Transplant = mongoose.model('Transplant', transplantSchema);
-
-module.exports = Transplant;
+module.exports = mongoose.model("Transplant", transplantSchema);
